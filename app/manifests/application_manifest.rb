aa771ef4007f9111574f2b67a22855f0073718f2
Bob Payne <bob@electroglide.biz>
2010-11-13 08:22:28 -0500

    server ailais

--- app/manifests/application_manifest.rb
+++ app/manifests/application_manifest.rb
@@ -53,7 +53,6 @@ class ApplicationManifest < Moonshine::Manifest::Rails
         :wordpress => {
           :directory => 'wp_momentumdance',
           :domain          => 'www.momentumdancetheatre.com',
-          :vhost_extra     => 'ServerAlias momentumdancetheater.com',  
           :auth_key        => 'Ga--au|1$-HYu@3;S7d*}~X`1dpG|C<4Q62hiu?t*cRe$3X8DswKj%~W{S(fZVA0',
           :secure_auth_key => '`ze`VMzYe|XM(3d;gziu7A=:fDAtOGULyX n+(g.|GYj~w<MZ/Es1`$TN| ttD!q',
           :logged_in_key   => 'kioPA/wUQ(l>j]&T0R^YV*JB)|Qj2(T.SMF_qNe*<RFZE{r]7Ib i|qGXgYx0fA:',
@@ -61,8 +60,8 @@ class ApplicationManifest < Moonshine::Manifest::Rails
           :db => {:name => 'wp_momentum' ,:username => 'wp_momentum' ,:password => 'wp_momentum'}
         }
       )
- 
-  # The following line includes the 'application_packages' recipe defined above
+ #     :vhost_extra     => 'ServerAlias momentumdancetheater.com',  
+ # The following line includes the 'application_packages' recipe defined above
   recipe :application_packages
   plugin :wordpress
   recipe :wordpress
